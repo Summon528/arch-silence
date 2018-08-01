44f9ec501291063c83822a50838f4d91c502b453
CodyTseng <cody880528@hotmail.com>
2018-08-01 14:15:04 +0800

    update install

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
-rm -rf /boot/grub/themes/arch-silence
-cp -TR ./theme /boot/grub/themes/arch-silence
-grub-mkconfig -o /boot/grub/grub.cfg
+#!/bin/bash
+
+if [ "$UID" -eq "0" ]; then
+	cp -TR ./theme /boot/grub/themes/arch-silence
+	grep "GRUB_THEME=" /etc/default/grub 2>&1 >/dev/null && sed -i '/GRUB_THEME=/d' /etc/default/grub
+	echo "GRUB_THEME=\"/boot/grub/themes/arch-silence/theme.txt\"" >> /etc/default/grub
+	grub-mkconfig -o /boot/grub/grub.cfg
+	echo -e "\n Finished! Hooray!"
+else
+	echo -e "\n Please run the script as root."
+fi
